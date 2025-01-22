bf5296c9670a0142ab942ac9e7d683ce1edb9ddf
Napatsawan Tosomnak <6710110206@psu.ac.th>
2025-01-22 14:54:49 +0700

    try to add clound

--- app/DinoRun.py
+++ app/DinoRun.py
@@ -9,34 +9,12 @@ from kivy.clock import Clock
 
 
 class Background(Widget):
-    image_one = ObjectProperty(Image)
-    image_two = ObjectProperty(Image)
-    image_three = ObjectProperty(Image)
-
-    velocity_x = NumericProperty(0)
-    # velocity = ReferenceListProperty(velocity_x)
-
-    def update(self):
-        self.image_one.pos = Vector(*self.velocity_x) + self.image_one.pos
-        self.image_two.pos = Vector(*self.velocity_x) + self.image_two.pos
-        self.image_three.pos = Vector(*self.velocity_x) + self.image_three.pos
-
-        if self.image_one.right <= 0:
-            self.image_one.pos = (self.width, 0)
-        if self.image_two.right <= 0:
-            self.image_two.pos = (self.width, 0)
-        if self.image_three.right <= 0:
-            self.image_three.pos = (self.width, 0)
-
-    def update_position(self):
-        self.image_one.pos = (0, 0)
-        self.image_two.pos = (self.width, 0)
-        self.image_three.pos = (self.width, 0)
+    clound_texture = ObjectProperty(None)
+    pass
 
 
 class DinoRunApp(App):
-    def build(self):
-        return Background()
+    pass
 
 
 if __name__ == "__main__":
