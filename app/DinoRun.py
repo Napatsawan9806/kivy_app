99273aca3db8af6944a8135bc025aa7c41377b1c
Napatsawan Tosomnak <6710110206@psu.ac.th>
2025-01-22 15:31:06 +0700

    finish clound animation

--- app/DinoRun.py
+++ app/DinoRun.py
@@ -3,6 +3,7 @@ import kivy
 from kivy.uix.widget import Widget
 from kivy.app import App
 from kivy.uix.image import Image
+from kivy.core.window import Window
 from kivy.properties import ObjectProperty, NumericProperty, ReferenceListProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
@@ -16,11 +17,23 @@ class Background(Widget):
 
         self.clound_texture = Image(source="clound.png").texture
         self.clound_texture.wrap = "repeat"
+        self.clound_texture.uvsize = (Window.width / self.clound_texture.width, -1)
+
+    def scroll_texture(self, time_passed):
+        self.clound_texture.uvpos = (
+            (self.clound_texture.uvpos[0] + time_passed) % Window.width,
+            self.clound_texture.uvpos[1],
+        )
+        texture = self.property("clound_texture")
+        texture.dispatch(self)
 
     pass
 
 
 class DinoRunApp(App):
+    def on_start(self):
+        Clock.schedule_interval(self.root.ids.background.scroll_texture, 1 / 7.5)
+
     pass
 
 
